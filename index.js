41e0b1860c4d0e625c45cdccd1115bbb0ca3316c
Lubin Pappalardo <lubinpappalardo@gmail.com>
2024-08-10 23:53:12 +0200

    10/08 progress

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+let isMobile;
 const img = $('#home_lubinpappalardo');
-const img_circle = $('#header_circle');
+const img_circle = $('#home_circle');
 let cursor = {x: 0, y: 0};
 
 $(window).on('mousemove', (event) => {
@@ -7,18 +8,54 @@ $(window).on('mousemove', (event) => {
     cursor.y = event.clientY;
 });
 
-const parallax = (element, parallaxOffset) => {
-    const x = (cursor.x - window.innerWidth / 2) /  (window.innerWidth / 2) * parallaxOffset;
-    const y = (cursor.y - window.innerHeight / 2) / (window.innerHeight / 2) * parallaxOffset;
+let img_circle_scale = 1;
+let img_circle_target_scale = 1;
 
-    element.css('transform', `translate(${x}%, ${y}%)`);
+const parallax = (element, parallaxOffset, offset, scale) => {
+    const x = ((cursor.x / (window.innerWidth * 0.5)) - 1) * parallaxOffset.x;
+    const y = ((cursor.y / (window.innerHeight * 0.5)) -1) * parallaxOffset.y;
+
+    element.css('transform', `translate(${x + offset.x}%, ${y + offset.y}%) scale(${scale})`);
 };
 
-const loop = () => {
-    parallax(img, 1);
-    parallax(img_circle, 7);
+const loop = () => {    
+    img_circle_scale += (img_circle_target_scale - img_circle_scale) * 0.2;
+
+    // Cancel parallax effect if mobile
+    let img_parallax_offset = !isMobile ? {x: 1, y: 0.2} : {x: 0, y: 0};
+    let img_circle_parallax_offset = !isMobile ? {x: 5, y: 2} : {x: 0, y: 0};
+
+    parallax(img, img_parallax_offset, {x: 0, y: 10}, 1);
+    parallax(img_circle, img_circle_parallax_offset, {x: 0, y: -140}, img_circle_scale);
+
     requestAnimationFrame(loop);
 };
 
 loop();
+
+$(window).on('mousedown', () => {
+    img_circle_target_scale = 1.1;
+});
+
+$(window).on('mouseup', () => {
+    img_circle_target_scale = 1;
+});
+
+// --------------------------------------------------------
+
+function isMobileFn() {
+    isMobile = window.innerWidth <= 768 || navigator.userAgent.match(/Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i);
+}
+
+$(window).on('resize', isMobileFn);
+isMobileFn();
+
+// --------------------------------------------------------
+
+const themeToggle = $('#theme_toggle');
+
+themeToggle.on('click', () => {
+    $('body').toggleClass('dark_mode');
+    themeToggle.toggleClass('dark_mode');
+});
 
