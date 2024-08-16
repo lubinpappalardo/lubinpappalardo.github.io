655dd215f3b8939e91fcd43a4a2ac2eb5b78287b
Lubin Pappalardo <lubinpappalardo@gmail.com>
2024-08-16 18:33:54 +0200

    changed everything

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 let isMobile;
-const img = $('#home_lubinpappalardo');
-const img_circle = $('#home_circle');
+const img = $('#home_section_image');
 let cursor = {x: 0, y: 0};
 
 $(window).on('mousemove', (event) => {
     cursor.x = event.clientX;
     cursor.y = event.clientY;
 });
-
-let img_circle_scale = 1;
-let img_circle_target_scale = 1;
 
 const parallax = (element, parallaxOffset, offset, scale) => {
     const x = ((cursor.x / (window.innerWidth * 0.5)) - 1) * parallaxOffset.x;
@@ -19,27 +15,15 @@ const parallax = (element, parallaxOffset, offset, scale) => {
 };
 
 const loop = () => {    
-    img_circle_scale += (img_circle_target_scale - img_circle_scale) * 0.2;
-
     // Cancel parallax effect if mobile
     let img_parallax_offset = !isMobile ? {x: 1, y: 0.2} : {x: 0, y: 0};
-    let img_circle_parallax_offset = !isMobile ? {x: 5, y: 2} : {x: 0, y: 0};
 
-    parallax(img, img_parallax_offset, {x: 0, y: 10}, 1);
-    parallax(img_circle, img_circle_parallax_offset, {x: 0, y: -140}, img_circle_scale);
+    parallax(img, img_parallax_offset, {x: 0, y: 0}, 1);
 
     requestAnimationFrame(loop);
 };
 
 loop();
-
-$(window).on('mousedown', () => {
-    img_circle_target_scale = 1.1;
-});
-
-$(window).on('mouseup', () => {
-    img_circle_target_scale = 1;
-});
 
 // --------------------------------------------------------
 
@@ -55,7 +39,64 @@ isMobileFn();
 const themeToggle = $('#theme_toggle');
 
 themeToggle.on('click', () => {
-    $('body').toggleClass('dark_mode');
-    themeToggle.toggleClass('dark_mode');
+    $('body').toggleClass('light_mode');
+    themeToggle.toggleClass('light_mode');
 });
+
+// --------------------------------------------------------
+
+const hamburgerMenu = $('#hamburger_menu');
+const mobileMenu = $('#mobile_menu'); 
+
+hamburgerMenu.on('click', () => {
+    hamburgerMenu.toggleClass('opened');
+    mobileMenu.toggleClass('opened');
+});
+
+// --------------------------------------------------------
+
+const hobbies = $('#hobbies');
+let hobbiesHover = false;
+let hobbiesCursor = $('#hobbies_cursor');
+
+const cursorHobbiesLoop = () => {    
+    // if (!hobbiesHover) {
+    //     return;
+    // }
+
+    const smoothness = 0.2; // Adjust this value for desired smoothness
+    const targetX = cursor.x;
+    const targetY = cursor.y;
+
+    const dx = (targetX - parseFloat(hobbiesCursor.css('left'))) * smoothness;
+    const dy = (targetY - parseFloat(hobbiesCursor.css('top'))) * smoothness;
+
+    const newX = parseFloat(hobbiesCursor.css('left')) + dx;
+    const newY = parseFloat(hobbiesCursor.css('top')) + dy;
+
+    hobbiesCursor.css('left', newX);
+    hobbiesCursor.css('top', newY);
+
+    requestAnimationFrame(cursorHobbiesLoop);
+};
+
+cursorHobbiesLoop();
+
+// hobbies.on('mouseenter', (event) => {
+//     if (isMobile || hobbiesHover) {
+//         return;
+//     }
+//     hobbiesCursor.css('left', cursor.x);
+//     hobbiesCursor.css('top', cursor.y);
+//     $('#home_section').append(hobbiesCursor);
+
+//     hobbiesHover = true;
+//     cursorHobbiesLoop();
+// });
+
+// hobbies.on('mouseleave', () => {
+//     console.log('leave');
+//     hobbiesHover = false;
+//     hobbiesCursor.remove();
+// });
 
